9447891a01510e6ae0f51e8d840bce50121b7596
LIRINE JOHN C <49216@fisat>
2018-04-18 01:19:26 -0400

    create function to add 4 numbers

--- add.py
+++ add.py
@@ -1,5 +1,7 @@
 x=int(raw_input("enter the num"));
 y=int(raw_input("enter the num"));
 a=int(raw_input("enter the num"));
-z=x+y+a;
+b=int(raw_input("enter the num"));
+z=x+y+a+b;
+
 print "sum is",z;
